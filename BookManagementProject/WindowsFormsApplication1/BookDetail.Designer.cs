ac0feaca495259a304c68d39818c8bbc20cb3e56
Healer2202 <0962392354Hl>
2019-12-14 15:49:31 +0700

    BookManagementProject

---
+++ BookManagementProject/WindowsFormsApplication1/BookDetail.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class BookDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BookDetail));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(256, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(267, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Đây là khi người dùng bấm vô sách";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(250, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 33);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Số lượt xem";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.Location = new System.Drawing.Point(250, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(209, 33);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "nhập điểm rate";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(305, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(218, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Đây là khi người dùng rate";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(425, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(410, 160);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(250, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 33);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Điểm rate";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(463, 90);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(25, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(204, 345);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BookDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 369);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Name = "BookDetail";
+            this.Text = "template";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ BookManagementProject/WindowsFormsApplication1/BookDetail.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class BookDetail : Form
+    {
+        public int count = 0;
+        private double rate = 0;
+        public BookDetail()
+        {
+            InitializeComponent();
+            this.button1.Click += button1_Click;
+            this.button2.Click += button2_Click;
+            this.textBox1.Text = count.ToString(); 
+        }
+   
+        void button2_Click(object sender, EventArgs e)
+        {
+            var diem = double.Parse(this.textBox2.Text); // lấy số
+            if (count == 1) // rate lần đầu = chính nó
+            {
+               this.textBox3.Text = diem.ToString();
+               rate = diem; // lưu điểm rate lần 1 
+            }
+            else // rate lần N
+            {
+                var result = (((diem + rate) / count) + ((diem + rate) % count)); 
+                rate =rate + result; // lấy rate cũ + rate mới 
+                
+                if (result > 10)
+                {
+                    this.textBox3.Text = "10";
+                }
+                else
+                {
+                    this.textBox3.Text = result.ToString();
+                }              
+            }           
+        }
+        void button1_Click(object sender, EventArgs e)
+        {
+            count++;
+            this.textBox1.Text = count.ToString(); // xong view nhé 
+        }
+    }
+}

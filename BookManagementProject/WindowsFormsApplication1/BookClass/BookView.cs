eaa09a6e02f027d95bcbe5420a78990252fcd39f
NguyenHieu20468 <hieu.187pm20468@vanlanguni.vn>
2019-12-16 23:09:46 +0700

    fix_again_Class

---
+++ BookManagementProject/WindowsFormsApplication1/BookClass/BookView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class BookView
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string AuthorCode { get; set; }
+        public string Topic { get; set; }
+
+        public BookView(Book bk)
+        {
+            this.Id = bk.ID;
+            this.Code = bk.BookCode;
+            this.Name = bk.BookName;
+            this.AuthorCode = bk.AuthorCode;
+            this.Topic = bk.Topic;
+        }
+    }
+}
---
+++ BookManagementProject/WindowsFormsApplication1/BookClass/LogicLayer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class LogicLayer
+    {
+        public Book[] GetStudents()
+        {
+            var db = new BookDatabaseEntities();
+            return db.Books.ToArray();
+        }
+
+        public Book GetBook(int id)
+        {
+            var db = new BookDatabaseEntities();
+            var bk = db.Books.Find();
+            return bk;
+        }
+        public void CreateBook(String code, string name, string topic, string Author_code)
+        {
+            var bk = new Book();
+            bk.BookCode = code;
+            bk.BookName = name;
+            bk.Topic = topic;
+            bk.AuthorCode = Author_code;
+
+            var db = new BookDatabaseEntities();
+            db.Books.Add(bk);
+            db.SaveChanges();
+        }
+
+        public void UpdateBook(int id, String code, string name, string topic, string Author_code)
+        {
+            var db = new BookDatabaseEntities();
+            var bk = db.Books.Find(id);
+
+            bk.BookCode = code;
+            bk.BookName = name;
+            bk.Topic = topic;
+            bk.AuthorCode = Author_code;
+
+            db.Entry(bk).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+        }
+        public void DeleteStudent(int id)
+        {
+            var db = new BookDatabaseEntities();
+            var bk = db.Books.Find(id);
+
+            db.Books.Remove(bk);
+            db.SaveChanges();
+        }
+        public Author[] GetAuthors()
+        {
+            var db = new BookDatabaseEntities();
+            return db.Authors.ToArray();
+        }
+    }
+}
